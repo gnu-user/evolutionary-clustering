a7d43650c00988042d516ab6bd73fe7984f5d4ce
gnu_user <gsoc.student@gmail.com>
2015-08-11 22:29:14 -0400

    Added crossover operator function

---
+++ include/operators.h
@@ -0,0 +1,48 @@
+/*
+ * Evolutionary K-means clustering (E-means) using Genetic Algorithms.
+ *
+ * Copyright (C) 2015, Jonathan Gillett
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef OPERATORS_H_
+#define OPERATORS_H_
+
+#include <gsl/gsl_matrix.h>
+#include "pcg_basic.h"
+
+/**
+ * Performs chromosome crossover by randomly selecting a crossover point and
+ * randomly either swapping the top or the bottom half.
+ * 
+ * @param  len     The length of the chromosome
+ * @param  parent1 The first parent chromosome
+ * @param  parent2 The second parent chromosome
+ * @param  rng     Pointer to the random number generator
+ */
+extern void crossover(gsl_matrix *parent1, gsl_matrix *parent2, pcg32_random_t *rng);
+
+
+/**
+ * Performs mutation, mutates a value randomly within the min/max bounds of the data.
+ * 
+ * @param  chromosome The chromosome
+ * @param  bounds     The min/max bounds for each dimensions of the data
+ * @param  rng        Pointer to the random number generator
+ */
+extern void mutate(gsl_matrix *chromosome, gsl_matrix *bounds, pcg32_random_t *rng);
+
+
+#endif /* OPERATORS_H_ */
---
+++ src/operators.c
@@ -0,0 +1,103 @@
+/*
+ * Evolutionary K-means clustering (E-means) using Genetic Algorithms.
+ *
+ * Copyright (C) 2015, Jonathan Gillett
+ * All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <float.h>
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_blas.h>
+#include <gsl/gsl_statistics.h>
+#include "utility.h"
+#include "fitness.h"
+#include "operators.h"
+
+void crossover(gsl_matrix *parent1, gsl_matrix *parent2, pcg32_random_t *rng)
+{
+    uint32_t rows = parent1->size1,
+             cols = parent1->size2;
+    uint32_t cut = (uint32_t)pcg32_boundedrand_r(rng, rows-1) + 1;
+
+    if (DEBUG == DEBUG_CROSSOVER)
+    {
+        printf(YELLOW "[SLAVE %2d] CROSSOVER CENTROIDS BEFORE\n" RESET, SLAVE);
+        printf(YELLOW "CHROMSOME CUT POINT: %d\n", cut);
+        printf(YELLOW "PARENT[1]:\n" RESET);
+        for (uint32_t i = 0; i < rows; ++i)
+        {
+            for (uint32_t j = 0; j < cols; ++j)
+            {
+                printf(YELLOW "%10.6f " RESET, gsl_matrix_get(parent1, i, j));
+            }
+            printf("\n");
+        }
+        printf(YELLOW "PARENT[2]:\n" RESET);
+        for (uint32_t i = 0; i < rows; ++i)
+        {
+            for (uint32_t j = 0; j < cols; ++j)
+            {
+                printf(YELLOW "%10.6f " RESET, gsl_matrix_get(parent2, i, j));
+            }
+            printf("\n");
+        }
+    }
+
+    // Copy bottom half from parent1 to parent2
+    for (uint32_t i = 0; i < cut; ++i)
+    {
+        gsl_vector_view parent1_row = gsl_matrix_row(parent1, i);
+        gsl_vector_view parent2_row = gsl_matrix_row(parent2, i);
+        gsl_vector_memcpy(&parent2_row.vector, &parent1_row.vector);
+    }
+
+    // Copy top half from parent2 to parent1
+    for (uint32_t i = cut; i < rows; ++i)
+    {
+        gsl_vector_view parent1_row = gsl_matrix_row(parent1, i);
+        gsl_vector_view parent2_row = gsl_matrix_row(parent2, i);
+        gsl_vector_memcpy(&parent1_row.vector, &parent2_row.vector);
+    }
+
+    if (DEBUG == DEBUG_CROSSOVER)
+    {
+        printf(YELLOW "[SLAVE %2d] CROSSOVER CENTROIDS AFTER\n" RESET, SLAVE);
+        printf(YELLOW "CHROMSOME CUT POINT: %d\n", cut);
+        printf(YELLOW "PARENT[1]:\n" RESET);
+        for (uint32_t i = 0; i < rows; ++i)
+        {
+            for (uint32_t j = 0; j < cols; ++j)
+            {
+                printf(YELLOW "%10.6f " RESET, gsl_matrix_get(parent1, i, j));
+            }
+            printf("\n");
+        }
+        printf(YELLOW "PARENT[2]:\n" RESET);
+        for (uint32_t i = 0; i < rows; ++i)
+        {
+            for (uint32_t j = 0; j < cols; ++j)
+            {
+                printf(YELLOW "%10.6f " RESET, gsl_matrix_get(parent2, i, j));
+            }
+            printf("\n");
+        }
+    }
+}
